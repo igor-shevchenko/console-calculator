4031f70b3f0cc540b4954f2f4f561220db011c0e
Igor Shevchenko <phar0s@ya.ru>
2012-12-16 03:06:00 +0600

    Интерфейс

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
 
 namespace ConsoleCalculator
 {
@@ -9,6 +11,32 @@ namespace ConsoleCalculator
     {
         static void Main(string[] args)
         {
+            using (var container = new ContainerFactory().GetContainer())
+            {
+                var calculator = container.Resolve<ICalculator>();
+                const string prompt = "> ";
+                Console.WriteLine("Type expression (e.g. \"1+2\") for calculation or \"exit\" for exit");
+                while (true)
+                {
+                    Console.Write(prompt);
+                    string command = Console.ReadLine();
+                    if (command == "exit")
+                        break;
+                    try
+                    {
+                        double result = calculator.Calculate(command);
+                        Console.WriteLine(result);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Division by zero exception");
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Syntax error");
+                    }
+                }
+            }
         }
     }
 }
