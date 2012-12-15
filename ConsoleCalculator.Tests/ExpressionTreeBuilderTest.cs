031dee0b609790c6c991baf2372b283c43904ccd
Igor Shevchenko <phar0s@ya.ru>
2012-12-16 01:00:05 +0600

    ExpressionTreeBuilder

---
+++ ConsoleCalculator.Tests/ExpressionTreeBuilderTest.cs
@@ -0,0 +1,91 @@
+using System.Collections.Generic;
+using ConsoleCalculator.Operations;
+using ConsoleCalculator.Tree;
+using NUnit.Framework;
+
+namespace ConsoleCalculator.Tests
+{
+    [TestFixture]
+    class ExpressionTreeBuilderTest
+    {
+        [Test]
+        public void TestBuildTree()
+        {
+            var tokens = new List<Token>
+                             {
+                                 new Token(1),
+                                 new Token(new AdditionOperator()),
+                                 new Token(2)
+                             };
+            var builder = new ExpressionTreeBuilder();
+
+            var tree = builder.Build(tokens);
+
+            Assert.AreEqual(tree.Token, tokens[1]);
+            Assert.AreEqual(tree.Children[0].Token, tokens[0]);
+            Assert.AreEqual(tree.Children[1].Token, tokens[2]);
+        }
+
+        [Test]
+        public void TestBuildWithUnaryOperation()
+        {
+            var tokens = new List<Token>
+                             {
+                                 new Token(new NegationOperator()),
+                                 new Token(1),
+                             };
+            var builder = new ExpressionTreeBuilder();
+
+            var tree = builder.Build(tokens);
+
+            Assert.AreEqual(tree.Token, tokens[0]);
+            Assert.AreEqual(tree.Children[0].Token, tokens[1]);
+        }
+
+        [Test]
+        public void TestBuildWithBrackets()
+        {
+            var tokens = new List<Token>
+                             {
+                                 new Token(1),
+                                 new Token(new AdditionOperator()),
+                                 new Token(Bracket.Opening),
+                                 new Token(2),
+                                 new Token(new AdditionOperator()),
+                                 new Token(3),
+                                 new Token(Bracket.Closing),
+                             };
+            var builder = new ExpressionTreeBuilder();
+
+            var tree = builder.Build(tokens);
+
+            Assert.AreEqual(tree.Token, tokens[1]);
+            Assert.AreEqual(tree.Children[0].Token, tokens[0]);
+            Assert.AreEqual(tree.Children[1].Token, tokens[4]);
+            Assert.AreEqual(tree.Children[1].Children[0].Token, tokens[3]);
+            Assert.AreEqual(tree.Children[1].Children[1].Token, tokens[5]);
+        }
+
+        [Test]
+        public void TestBuildWithCorrectPrecedence()
+        {
+            var tokens = new List<Token>
+                             {
+                                 new Token(1),
+                                 new Token(new AdditionOperator()),
+                                 new Token(2),
+                                 new Token(new MultiplicationOperator()),
+                                 new Token(3),
+                             };
+            var builder = new ExpressionTreeBuilder();
+
+            var tree = builder.Build(tokens);
+
+            Assert.AreEqual(tree.Token, tokens[1]);
+            Assert.AreEqual(tree.Children[0].Token, tokens[0]);
+            Assert.AreEqual(tree.Children[1].Token, tokens[3]);
+            Assert.AreEqual(tree.Children[1].Children[0].Token, tokens[2]);
+            Assert.AreEqual(tree.Children[1].Children[1].Token, tokens[4]);
+        }
+    }
+}
---
+++ ConsoleCalculator/Tree/ExpressionTreeBuilder.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ConsoleCalculator.Tree
+{
+    public class ExpressionTreeBuilder
+    {
+        public ExpressionTree Build(List<Token> tokens)
+        {
+            if (tokens.Count == 0)
+                return null;
+            if (IsExpressionInBrackets(tokens))
+            {
+                return Build(tokens.GetRange(1, tokens.Count - 2));
+            }
+            if (IsExpressionASingleValue(tokens))
+            {
+                return new ExpressionTree(tokens[0], null);
+            }
+            var index = GetIndexOfLeastPrecedentOperation(tokens);
+            var leftchild = Build(tokens.GetRange(0, index));
+            var rightchild = Build(tokens.GetRange(index + 1, tokens.Count - 1 - index));
+            var children = new List<IExpressionTree>();
+            if (leftchild != null)
+                children.Add(leftchild);
+            if (rightchild != null)
+                children.Add(rightchild);
+            return new ExpressionTree(tokens[index], children);
+        }
+
+        private bool IsExpressionInBrackets(IList<Token> tokens)
+        {
+            var count = tokens.Count;
+            if (count < 2)
+                return false;
+            var startsWithOpeningBracket = tokens[0].IsBracket && tokens[0].GetBracket() == Bracket.Opening;
+            var endsWithClosingBracket = tokens[count - 1].IsBracket && tokens[count - 1].GetBracket() == Bracket.Closing;
+            return startsWithOpeningBracket && endsWithClosingBracket;
+        }
+
+        private bool IsExpressionASingleValue(IList<Token> tokens)
+        {
+            if (tokens.Count > 1)
+                return false;
+            return tokens[0].IsValue;
+        }
+
+        private int GetIndexOfLeastPrecedentOperation(IList<Token> tokens)
+        {
+            var leastIndex = -1;
+            var bracketDepth = 0;
+            for (var i = 0; i < tokens.Count; ++i)
+            {
+                var token = tokens[i];
+                if (token.IsBracket)
+                {
+                    if (token.GetBracket() == Bracket.Opening)
+                        bracketDepth++;
+                    else
+                        bracketDepth--;
+                    continue;
+                }
+                if (bracketDepth > 0)
+                    continue;
+
+                if (token.IsBinaryOperator || token.IsUnaryOperator)
+                {
+                    if (leastIndex == -1 || GetOperatorPrecedence(token) < GetOperatorPrecedence(tokens[leastIndex]))
+                    {
+                        leastIndex = i;
+                    }
+                }
+            }
+            return leastIndex;
+        }
+
+        private int GetOperatorPrecedence(Token op)
+        {
+            if (op.IsBinaryOperator)
+                return op.GetBinaryOperator().Precedence;
+            if (op.IsUnaryOperator)
+                return op.GetUnaryOperator().Precedence;
+            throw new Exception();
+        }
+    }
+}
