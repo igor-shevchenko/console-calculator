c8add4db5cfdd0aae8894b3663fddac38621ec76
Igor Shevchenko <phar0s@ya.ru>
2012-12-16 01:51:13 +0600

    Класс Calculator для вычисления значения выражения

---
+++ ConsoleCalculator.Tests/CalculatorTest.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using ConsoleCalculator.Tree;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace ConsoleCalculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void TestCalculate()
+        {
+            var expression = String.Empty;
+            var splittedString = new List<string>();
+            var tokens = new List<Token>();
+            var value = 0;
+
+            var splitter = MockRepository.GenerateMock<ISplitter>();
+            var tokenizer = MockRepository.GenerateMock<ITokenizer>();
+            var builder = MockRepository.GenerateMock<IExpressionTreeBuilder>();
+            var tree = MockRepository.GenerateMock<IExpressionTree>();
+
+            splitter.Expect(s => s.Split(Arg<string>.Is.Equal(expression))).Return(splittedString);
+            tokenizer.Expect(t => t.Tokenize(Arg<List<string>>.Is.Equal(splittedString))).Return(tokens);
+            builder.Expect(b => b.Build(Arg<List<Token>>.Is.Equal(tokens))).Return(tree);
+            tree.Expect(t => t.GetResult()).Return(value);
+            
+            var calculator = new Calculator(splitter, tokenizer, builder);
+
+            var result = calculator.Calculate(expression);
+
+            Assert.AreEqual(value, result);
+
+            splitter.VerifyAllExpectations();
+            tokenizer.VerifyAllExpectations();
+            builder.VerifyAllExpectations();
+            tree.VerifyAllExpectations();
+        }
+    }
+}
---
+++ ConsoleCalculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleCalculator.Tree;
+
+namespace ConsoleCalculator
+{
+    public class Calculator
+    {
+        private readonly ISplitter splitter;
+        private readonly ITokenizer tokenizer;
+        private readonly IExpressionTreeBuilder expressionTreeBuilder;
+
+        public Calculator(ISplitter splitter, ITokenizer tokenizer, IExpressionTreeBuilder expressionTreeBuilder)
+        {
+            this.splitter = splitter;
+            this.tokenizer = tokenizer;
+            this.expressionTreeBuilder = expressionTreeBuilder;
+        }
+
+        public double Calculate(string s)
+        {
+            var splittedString = splitter.Split(s).ToList();
+            var tokens = tokenizer.Tokenize(splittedString).ToList();
+            var expressionTree = expressionTreeBuilder.Build(tokens);
+            return expressionTree.GetResult();
+        }
+    }
+}
